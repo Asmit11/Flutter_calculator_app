d5168b9e7ca4df0b8af76d355bc10cb1627604ba
Sandeep <sandeep@Sandeeps-MacBook-Air.local>
2024-05-07 11:22:03 +0545

    changes

--- lib/screen/calculator_screen.dart
+++ lib/screen/calculator_screen.dart
@@ -9,21 +9,12 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  final gap = const SizedBox(
-    height: 10,
-  );
-
-  int? first;
-
-  int? second;
-
-  int result = 0;
-
   final TextEditingController _numController = TextEditingController();
-
-  final List<String> IstOperatorNumber = [
+  int first = 0;
+  int second = 0;
+  final List<String> lstOperatorNumber = [
     "C",
-    "",
+    "*",
     "/",
     "<-",
     "7",
@@ -37,78 +28,105 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     "1",
     "2",
     "3",
-    "",
+    "*",
     "/",
     "0",
     ".",
     "=",
   ];
 
+  final List<String> lstOperator = [
+    "+",
+    "-",
+    "*",
+    "/",
+  ];
+
+  String operator = "";
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text(
-            "Calculator App",
-            style: TextStyle(fontSize: 25),
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextFormField(
-                style: const TextStyle(fontSize: 30),
-                textAlign: TextAlign.end,
-                controller: _numController,
-                // limit the character to 10 digit
-                maxLength: 10,
-                // show the num keyboard
-                keyboardType: TextInputType.number,
-                // don't allow character typing
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '0',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "0";
-                  }
-                  return null;
-                },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              style: const TextStyle(fontSize: 30),
+              textAlign: TextAlign.end,
+              controller: _numController,
+              // limit the character to 10 digit
+              maxLength: 10,
+              // show the num keyboard
+              keyboardType: TextInputType.number,
+              // do not allow charater typing
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              decoration: const InputDecoration(
+                hintText: '0',
+                border: OutlineInputBorder(),
               ),
-              // gap,
-              Expanded(
-                  child: GridView.builder(
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Expanded(
+              child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 4,
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
                 ),
-                itemCount: IstOperatorNumber.length,
+                itemCount: lstOperatorNumber.length,
                 itemBuilder: (context, index) {
                   return ElevatedButton(
-                      onPressed: () {
-                        if (IstOperatorNumber[index] == "C") {
+                    onPressed: () {
+                      if (lstOperatorNumber[index] == "C") {
+                        _numController.text = "";
+                      } else if (lstOperatorNumber[index] == "<-") {
+                        _numController.text = _numController.text
+                            .substring(0, _numController.text.length - 1);
+                      } else if (lstOperator
+                          .contains(lstOperatorNumber[index])) {
+                        if (lstOperatorNumber[index] == "+" ||
+                            lstOperatorNumber[index] == "-" ||
+                            lstOperatorNumber[index] == "*" ||
+                            lstOperatorNumber[index] == "/") {
+                          // Set the operator
+                          operator = lstOperatorNumber[index];
+                          // set the first value to first variable
+                          first = int.parse(_numController.text);
                           _numController.text = "";
-                        } else if (IstOperatorNumber[index] == "<-") {
-                          _numController.text = _numController.text
-                              .substring(0, _numController.text.length - 1);
-                        } else {
-                          _numController.text += IstOperatorNumber[index];
                         }
-                      },
-                      child: Text(
-                        IstOperatorNumber[index],
-                        style: const TextStyle(fontSize: 30),
-                      ));
+                      } else if (lstOperatorNumber[index] == "=") {
+                        second = int.parse(_numController.text);
+
+                        // check for the operator and do the calculation
+                        if (operator == "+") {
+                          _numController.text = (first + second).toString();
+                        } else if (operator == "-") {
+                          _numController.text = (first - second).toString();
+                        } else if (operator == "*") {
+                          _numController.text = (first * second).toString();
+                        } else if (operator == "/") {
+                          _numController.text = (first / second).toString();
+                        }
+                      } else {
+                        _numController.text += lstOperatorNumber[index];
+                      }
+                    },
+                    child: Text(
+                      lstOperatorNumber[index],
+                      style: const TextStyle(fontSize: 30),
+                    ),
+                  );
                 },
-              ))
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
